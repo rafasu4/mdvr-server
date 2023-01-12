e2f9111b6e6cc2278ab687a6852d4e7c3ff46882
yuvalbtz <yuvalbentzur1996@gmail.com>
2023-01-12 02:02:38 +0200

    ver

--- server.py
+++ server.py
@@ -373,5 +373,9 @@ class ConsensusUnderDeadline():
 
 
 
+# if __name__ == "__main__":
+    # app.run(debug=True, host='0.0.0.0', port= 8888)
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port= 8888)
+    app.run(debug=True)
